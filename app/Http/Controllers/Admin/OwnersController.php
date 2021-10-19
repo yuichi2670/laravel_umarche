5cbe1f0af6bef7bb86baf54a45b0be64340fec73
yuichi tao <yuichitao2670@gmail.com>
2021-10-19 20:40:50 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner; //Eloquent
 use Illuminate\Support\Facades\DB; //Querybuilder
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class OwnersController extends Controller
 {
@@ -58,7 +60,22 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'string', 'confirmed', 'min:8', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+            ->route('admin.owners.index')
+            ->with('message', 'オーナー登録を実施しました。');
     }
 
     /**
