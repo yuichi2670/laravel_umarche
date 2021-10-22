1d066fb433f3b0c56a1c183611ff75062ac9218e
yuichi tao <yuichitao2670@gmail.com>
2021-10-22 22:32:03 +0900

    edit, update

--- resources/views/admin/owners/edit.blade.php
+++ resources/views/admin/owners/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            オーナー登録
+            オーナー情報編集
         </h2>
     </x-slot>
 
@@ -12,23 +12,24 @@
                     <section class="text-gray-600 body-font relative">
                         <div class="container px-5 mx-auto">
                           <div class="flex flex-col text-center w-full mb-12">
-                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー登録</h1>
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー情報編集</h1>
                           </div>
                           <div class="lg:w-1/2 md:w-2/3 mx-auto">
                             <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                            <form action="{{ route('admin.owners.store') }}" method="post">
+                            <form action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}" method="post">
+                              @method('PUT')
                               @csrf
                               <div class="-m-2">
                                 <div class="p-2 w-1/2 mx-auto">
                                   <div class="relative">
                                     <label for="name" class="leading-7 text-sm text-gray-600">オーナー名</label>
-                                    <input type="text" id="name" name="name" value="{{ old('name') )}}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <input type="text" id="name" name="name" value="{{ $owner->name }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                   </div>
                                 </div>
                                 <div class="p-2 w-1/2 mx-auto">
                                   <div class="relative">
                                     <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                    <input type="email" id="email" name="email" value="{{ old('email') }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <input type="email" id="email" name="email" value="{{ $owner->email }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                   </div>
                                 </div>
                                 <div class="p-2 w-1/2 mx-auto">
@@ -45,7 +46,7 @@
                                 </div>
                                 <div class="p-2 w-full flex justify-around mt-4">
                                   <button type="button" onclick="location.href='{{ route('admin.owners.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                                  <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                  <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                 </div>
                               </div>
                             </form>
